9ea234b75e8f86b467a451131ad416fbef9c1e17
김효은 <hyoeun@gmail.com>
2024-03-29 13:40:27 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import Box from './component/Box';
 
 function App() {
+  let count = useSelector(state => state.count);
+  let id = useSelector(state => state.id);
+  let password = useSelector(state => state.password);
+  const dispatch = useDispatch();
+
+  const increase = () => {
+    dispatch({type: "INCREMENT", payload: {num:1}});
+  }
+  const decement = () => {
+    dispatch({type: "DECREMENT", payload: {num:1}});
+  }
+  const login = () => {
+    dispatch({type: "Login", payload: {id:"noona",password:"123"}})
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{id},{password}</h1>
+      <h1>{count}</h1>
+      <button onClick={increase}>증가!</button>
+      <button onClick={login}>Login</button>
+      <button onClick={decement}>감소!</button>
+      <Box />
     </div>
   );
 }
---
+++ src/reducer/reducer.js
@@ -0,0 +1,17 @@
+let initialState = {
+    count:0,
+    id :"",
+    password: ""    
+}
+function reducer(state=initialState, action){
+   if(action.type==="INCREMENT"){
+    return {...state, count: state.count + action.payload.num};
+   } else if(action.type === "Login"){
+    return {...state, id: action.payload.id, password: action.payload.password}
+   } else if(action.type === "DECREMENT"){
+    return {...state, count: state.count - action.payload.num}
+   }
+   return {...state};
+}
+
+export default reducer;
